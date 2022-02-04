22295648658dc669d4cab42f5164b3b797203bd6
Henok <henokcodes@gmail.com>
2022-02-04 02:10:00 -0800

    java project setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# java-data-structures
-A playground to play around the different data structures like Stack, Queue, List, Hash and Tree using Java. 
---
+++ src/data/structures/ArrayList.java
@@ -0,0 +1,9 @@
+package data.structures;
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello");
+}
+}
