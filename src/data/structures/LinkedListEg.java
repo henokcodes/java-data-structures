67954836b04b2f4ac1335b249b6f5df324878bd7
Henok <henokcodes@gmail.com>
2022-02-08 11:05:33 -0800

    Linked list

--- src/data/structures/LinkedListEg.java
+++ src/data/structures/LinkedListEg.java
@@ -1,10 +1,65 @@
 package data.structures;
 
-public class LinkedListEg {
+public class LinkedListEg<T extends Comparable<T>> implements List<T> {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+  
+	}
 
+	private Node<T> root;
+	private int numOfItems;
+	
+	@Override
+	public void insert(T data) {
+		// TODO Auto-generated method stub
+		if(root==null) {
+			root = new Node<>(data);
+		}else {
+			insertBeginning(data);
+		}
+		
+	}
+	
+	public void insertBeginning(T data) {
+		Node<T> newNode = new Node<>(data);
+		newNode.setNextNode(root);
+		root = newNode;
+	}
+	
+	public void insertEnd(T data, Node<T> node) {
+		if(node.getNextNode() != null) {
+			insertEnd(data, node.getNextNode());
+		}else {
+			//last node
+			Node<T> newNode = new Node<>(data);
+			newNode.setNextNode(newNode);
+		}
+	}
+
+	@Override
+	public void remove(T data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void traverse() {
+		// TODO Auto-generated method stub
+		if(root!=null) return;
+		Node<T> actualNode = root;
+		
+		while(actualNode!= null) {
+			System.out.println(actualNode);
+			actualNode = actualNode.getNextNode();
+		}
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
---
+++ src/data/structures/List.java
@@ -0,0 +1,9 @@
+package data.structures;
+
+public interface List<T> {
+	public void insert(T data);
+	public void remove(T data);
+	public void traverse();
+	public int size();
+	 
+}
